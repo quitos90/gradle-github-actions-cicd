6fd70ae2c19f126e5db977884bccc90ed3340f70
Martín Alvarez, Marcos <mmartinal@eservicios.indra.es>
2025-06-11 17:05:20 +0200

    use github actions

---
+++ src/main/java/com/github_actions/gradle_github_actions_cicd/GradleGithubActionsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.github_actions.gradle_github_actions_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleGithubActionsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleGithubActionsCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github_actions/gradle_github_actions_cicd/GradleGithubActionsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github_actions.gradle_github_actions_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleGithubActionsCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
